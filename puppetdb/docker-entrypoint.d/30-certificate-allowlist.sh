d959c85e5842f0b41948d894c1e69d253e969e54
Martin Alfke <ma@betadots.de>
2025-02-04 01:18:40 +0100

    only set certificate-allowlist if configured
    fixes: #103

--- puppetdb/docker-entrypoint.d/30-certificate-allowlist.sh
+++ puppetdb/docker-entrypoint.d/30-certificate-allowlist.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+hocon() {
+  /opt/puppetlabs/puppet/lib/ruby/vendor_gems/bin/hocon "$@"
+}
+
 if [ "$PUPPETDB_CERTIFICATE_ALLOWLIST" != "" ]; then
+  hocon -f /etc/puppetlabs/puppetdb/conf.d/puppetdb.conf set puppetdb.certificate-allowlist certificate-allowlist
   IFS=','
   for cert in $PUPPETDB_CERTIFICATE_ALLOWLIST; do
     echo $cert >> /opt/puppetlabs/server/apps/puppetdb/certificate-allowlist
   done
-else
-  touch /opt/puppetlabs/server/apps/puppetdb/certificate-allowlist
 fi
 
